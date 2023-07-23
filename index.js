4b1ffac60d91643f763e5ba1e75c0b9e28cb1eee
Basharat khan <bk173498@gmail.com>
2023-07-23 13:55:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("ul-menu");
+
+function myFunction() {
+    
+
+    // var x = document.getElementById("ul-menu");
+    if (x.style.display == "none" ) {
+        x.style.display = "block";
+    }
+    else  {
+        x.style.display = "none";
+
+    }
+}
